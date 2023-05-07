b6b1d7e30c13cdf0eabc5672cc481895ed2f821c
HUANJU.LU <huanjulu@huanjus-MBP.localdomain>
2023-05-07 02:00:38 -0700

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Customer } from '../Customer';
 import { User } from '../User';
 
 @Component({
@@ -13,7 +14,7 @@ export class LoginComponent implements OnInit {
 
   showMessage=false;
   mainbody=true;
-  users!: User[];
+
   ngOnInit(): void {
   }
   constructor(private router: Router, private http: HttpClient) {}
@@ -21,31 +22,25 @@ export class LoginComponent implements OnInit {
   get f(){
     return this.profileForm.controls;
   }
-  
-  
   profileForm = new FormGroup({
     username: new FormControl('', [Validators.required, Validators.minLength(3)]),
     password: new FormControl('', [Validators.required, Validators.email]),
-    
   });
   user: User= new User();
+
+  apiUrl = 'http://127.0.0.1:8080/api/customer/login/'
+  url:string=''
+
   login() {
-    // this.user.name=this.f['username'].value;
-    // this.user.pwd=this.f['password'].value;
-    // console.log(this.profileForm.value);
-
-    // this.service.addPerson(this.user).subscribe(response => {
-    //   console.log("Receive the Response from server" + response);
-    // });
-
-    // this.showMessage=true;
-    // this.mainbody=false;
-
-    // this.http.get<User[]>('http://localhost:8080/getAllUsers').subscribe(users => {
-    //   this.users = users;
-    // });
-   
-    // this.router.navigate(['/home'])
-    // perform authentication logic here
-  }
+    this.user.name=this.f['username'].value;
+    this.user.pwd=this.f['password'].value;
+    this.url=this.apiUrl+this.user.name+'/'+this.user.pwd;
+    this.http.get<HttpResponse<any>>(this.url).subscribe((result) => {
+      if(result==null){
+        this.showMessage=true;
+      }else{
+        this.router.navigate(['profile']);
+      }
+    });
+}
 }
